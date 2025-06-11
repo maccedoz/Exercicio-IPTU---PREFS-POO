61e04b403c3f1a117fa609a5c3e7009026f795bd
Rian Victor <151791087+Rian-Victor@users.noreply.github.com>
2025-06-11 15:39:34 -0300

    Update Prefeitura.java

--- Prefeitura.java
+++ Prefeitura.java
@@ -1,1 +1,33 @@
+public class Prefeitura {
+    private int idadeMinimaDesconto1;
+    private double percentualDesconto1;
+    private int idadeMinimaDesconto2;
+    private double percentualDesconto2;
 
+    public Prefeitura() {
+        this.idadeMinimaDesconto1 = 0;
+        this.percentualDesconto1 = 0.0;
+        this.idadeMinimaDesconto2 = 0;
+        this.percentualDesconto2 = 0.0;
+    }
+
+    public void configurarDesconto1(int idadeMinima, double percentual) {
+        this.idadeMinimaDesconto1 = idadeMinima;
+        this.percentualDesconto1 = percentual / 100.0;
+    }
+
+    public void configurarDesconto2(int idadeMinima, double percentual) {
+        this.idadeMinimaDesconto2 = idadeMinima;
+        this.percentualDesconto2 = percentual / 100.0;
+    }
+
+    public double calcularFatorDesconto(int idadeResponsavel) {
+        if (this.idadeMinimaDesconto2 > 0 && idadeResponsavel >= this.idadeMinimaDesconto2) {
+            return this.percentualDesconto2;
+        }
+        if (this.idadeMinimaDesconto1 > 0 && idadeResponsavel >= this.idadeMinimaDesconto1) {
+            return this.percentualDesconto1;
+        }
+        return 0.0;
+    }
+}
