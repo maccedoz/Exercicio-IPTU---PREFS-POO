18e389de762da25df8733e08e581fd87a69fd278
Rian Victor <151791087+Rian-Victor@users.noreply.github.com>
2025-06-11 15:40:09 -0300

    Create Responsavel.java

---
+++ Responsavel.java
@@ -0,0 +1,23 @@
+public class Responsavel {
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    public Responsavel(String nome, String cpf, int idade) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+}
